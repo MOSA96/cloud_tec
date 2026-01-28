46198df8e3a0d87775d825673f6d567e2dc22f9b
alfredo <bt.queso96@gmail.com>
2026-01-27 23:44:06 -0600

    Generate random values for arrays and print first 10 elements of teach array

--- fast.cpp
+++ fast.cpp
@@ -1,43 +1,52 @@
 #include <iostream>
 #include <chrono>
 #include <omp.h>
+#include <random>
 
 #define N 100000
-#define chunk 1000
-#define hilos 8
+#define hilos 16
 
 int main(){
     float a[N], b[N], c[N];
     int i;
+    std::random_device rd;
+    std::uniform_real_distribution<float> dist(-1000.0f, 1000.0f);
 
-    for (i=0; i < N; i++)
-    {
-        a[i] = i;
-        b[i] = i*2;
+    for (i = 0; i < N; i++) {
+        a[i] = dist(rd);
+        b[i] = dist(rd);
     }
 
-    int pedazos = chunk;
 
     using clock = std::chrono::steady_clock;
     auto start = clock::now();
 
     //Hasta aquí la lógica es la misma que ejemplo secuencial
     omp_set_num_threads(hilos);
-    #pragma omp parallel shared(a, b, c, pedazos) private(i)
-    {
-        for (int repeat = 0; repeat < 10000; repeat++) {
-            #pragma omp for schedule(static, pedazos)
-            for (i = 0; i < N; i++) {
-                c[i] = a[i] + b[i];
-            }
+    #pragma omp parallel for
+    for (int repeat = 0; repeat < 10000; repeat++) {
+        for (i = 0; i < N; i++) {
+            c[i] = a[i] + b[i];
         }
     }
+    
 
     auto end = clock::now();
     auto time = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
 
     std::cout << "Elapsed: " << time << " milliseconds\n";
-    
+
+    std::cout << "a = ";
+    for (i = 0; i < 10; i++) {
+        std::cout << a[i] << " ";
+    }
+    std::cout << "\n"; 
+
+    std::cout << "b = ";
+    for (i = 0; i < 10; i++) {
+        std::cout << b[i] << " ";
+    }
+    std::cout << "\n"; 
     std::cout << "c = ";
     for (i = 0; i < 10; i++) {
         std::cout << c[i] << " ";
--- slow.cpp
+++ slow.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <chrono>
+#include <random>
 
 #define N 100000
 
 int main(){
     float a[N], b[N], c[N];
     int i;
+    std::random_device rd;
+    std::uniform_real_distribution<float> dist(-1000.0f, 1000.0f);
 
-    for (i=0; i < N; i++)
-    {
-        a[i] = i;
-        b[i] = i*2;
+    for (i = 0; i < N; i++) {
+        a[i] = dist(rd);
+        b[i] = dist(rd);
     }
 
     using clock = std::chrono::steady_clock;
@@ -30,6 +32,18 @@ int main(){
 
     std::cout << "Tiempo: " << time << " ms\n";
     
+    std::cout << "a = ";
+    for (i = 0; i < 10; i++) {
+        std::cout << a[i] << " ";
+    }
+    std::cout << "\n"; 
+
+    std::cout << "b = ";
+    for (i = 0; i < 10; i++) {
+        std::cout << b[i] << " ";
+    }
+    std::cout << "\n"; 
+
     std::cout << "c = ";
     for (i = 0; i < 10; i++) {
         std::cout << c[i] << " ";
